def66b5874e2c00b3334fdb614367638cc63fd3b
Mariosam2 <marco.mariosaa@gmail.com>
2023-01-13 16:42:41 +0100

    add placeholder.webp to seed fake images

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,8 +6,8 @@ use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
-use Faker\Factory as ImgFaker;
 use Illuminate\Support\Str;
+
 
 class ProjectSeeder extends Seeder
 {
@@ -19,15 +19,14 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
 
-        $img_faker = ImgFaker::create();
-        $img_faker->addProvider(new \Xvladqt\Faker\LoremFlickrProvider($img_faker));
+        // $faker->addProvider(new \Xvladqt\Faker\LoremFlickrProvider($faker));
         for ($i = 0; $i < 10; $i++) {
             $project = new Project();
             $project->title =  ucfirst($faker->words(3, true));
             $project->slug = Str::slug($project->title);
             $project->description = $faker->text();
             $project->creation_date = $faker->date();
-            $project->img = 'images/' . $img_faker->image('storage/app/public/images', 600, 300, ['cats'], false); //placeholders/sjfdposjadfgpojsdpfo.png
+            $project->img = 'images/' . 'placeholder.webp';
             $project->save();
         }
     }
